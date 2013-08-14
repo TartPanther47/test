0ba227af481168b40857f2f28cd358133af72e67
TartPanther47 <cfmgp@hotmail.com>
2013-08-14 13:32:40 -0400

    Added JFrame + Label("Lol");

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,12 +1,23 @@
 package test;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class Main {
+	
+	static JFrame frame = new JFrame("Game");
+	
 	public static void main(String args[]) {
 		
 		System.out.println("I am a gitrepo test! :) ");
 		
 		System.out.println("lol");
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(640, 480);
+		frame.setLocationRelativeTo(null);
+		frame.add(new JLabel("Lol"));
+		frame.setVisible(true);
 		
 	}
 }
